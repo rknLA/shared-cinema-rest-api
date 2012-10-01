39fca26422a5b063c3c213b4d010aae30c7bd7a5
rknLA <kevin@rkn.la>
2012-09-30 20:00:32 -0700

    Beginnings of a rails app...

---
+++ README.md
@@ -0,0 +1,1 @@
+Shared Cinema RESTful API
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SharedCinema::Application.config.secret_token = '832da0c71d2d696c66e2ea680376588389acb461f5c13b460fb025a2d095fbe2f85a4cace0fc0d90c6365817c5a71fb801d024575d3ef914e6af9e8abc07b69e'
